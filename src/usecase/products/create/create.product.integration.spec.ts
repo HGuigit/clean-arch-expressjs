ba753506aa8adad3ffba41fe2d3fa3529f2cde30
Guilherme de Souza (hguigit) <guilherme.silva@technosoftware.com.br>
2024-10-21 00:13:51 -0300

    feat: Adding integration tests for create product

---
+++ src/usecase/products/create/create.product.integration.spec.ts
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import { InputCreateProductDto } from "./create.product.dto";
+import CreateProductUseCase from "./create.product.usecase";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+
+
+
+
+describe('Unit tests for product use case', () => {
+    let input: InputCreateProductDto;
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false
+        });
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync(); 
+        input = {
+            name: 'Coca cola',
+            price: 10.00
+        };
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it('should create a product', async () => {
+        const productRepo = new ProductRepository();
+        const useCase = new CreateProductUseCase(productRepo);
+        const output = await useCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        })
+
+    })
+
+})
